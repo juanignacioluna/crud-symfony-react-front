25e49d527ac4d48f3636d9d50f141ac6c7e2fb63
Juan Ignacio Luna <chinoluna11@gmail.com>
2021-01-15 01:33:37 -0300

    Updates

---
+++ precache-manifest.e3b858821a298030143615cb488cc5e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba05f41c05f2d8eff53f366baacd931",
+    "url": "/crud-symfony-react-front/index.html"
+  },
+  {
+    "revision": "29db292081e8fe9abd95",
+    "url": "/crud-symfony-react-front/static/css/main.dd6f0f13.chunk.css"
+  },
+  {
+    "revision": "6d0b1604137ba956d570",
+    "url": "/crud-symfony-react-front/static/js/2.993e5e6a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/crud-symfony-react-front/static/js/2.993e5e6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29db292081e8fe9abd95",
+    "url": "/crud-symfony-react-front/static/js/main.e49f02b2.chunk.js"
+  },
+  {
+    "revision": "d20c62edf7de3abd2dff",
+    "url": "/crud-symfony-react-front/static/js/runtime-main.50db0f73.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/crud-symfony-react-front/static/media/logo.5d5d9eef.svg"
+  }
+]);
